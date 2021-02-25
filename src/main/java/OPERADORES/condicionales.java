a817e161fb16879b1e169d4985c62b779951b9cb
MAURICIO <u20132121837@usco.edu.co>
2021-02-25 17:52:37 -0500

    SE CREA CLASE IGUALDDA

---
+++ src/main/java/OPERADORES/condicionales.java
@@ -0,0 +1,10 @@
+package OPERADORES;
+
+public class condicionales {
+    public static void main(String[] args) {
+
+        int a=15, valMin=2, valMax=12;
+        boolean b= a>=valMin && a<=valMax;
+        System.out.print(b);
+    }
+}
